3a224a994b1acc39feb49ceadc4265b44eb8ac76
김태훈 <kth9193@gmail.com>
2025-08-15 17:17:13 +0900

    Update page.tsx

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -13,7 +13,7 @@ function LoginPage() {
   const { mutateAsync, isPending,  } = usePostGuestLogin({
     onCreateSuccess: () => {
       showToast.success("로그인 성공");
-      router.refresh();
+      router.replace('/');
     },
     onCreateError: (error) => {
       showToast.error("로그인 실패");
