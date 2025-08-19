aa43851b03bb70bb559db8db40f00a172047c52d
김태훈 <kth9193@gmail.com>
2025-08-19 15:59:53 +0900

    관리자 계정 - 구글 추가

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,23 +1,23 @@
 "use client";
 
+import GoogleSignInButton from "@/components/button/googleLoginBtn";
 import CustomSpinner from "@/components/spinner/customSpinner";
 import { showToast } from "@/components/toast/customToast";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { authClient } from "@/lib/auth-clinet";
 import { usePostGuestLogin } from "@/lib/hooks/query/usePostGuestLogin";
 import { useRouter } from "next/navigation";
 
 function LoginPage() {
   const router = useRouter();
 
-  const { mutateAsync, isPending,  } = usePostGuestLogin({
+  const { mutateAsync, isPending } = usePostGuestLogin({
     onCreateSuccess: () => {
-      
       setTimeout(() => {
-        showToast.success("로그인 성공");  
-        router.push('/');
+        showToast.success("로그인 성공");
+        router.push("/");
       }, 1000);
-      
     },
     onCreateError: (error) => {
       showToast.error("로그인 실패");
@@ -36,7 +36,7 @@ function LoginPage() {
 
   return (
     <section className="flex justify-center items-center h-screen bg-gray-50">
-      <div className="w-full max-w-sm mx-auto">
+      <div className="flex flex-col w-full max-w-sm mx-auto">
         <div className="text-center mb-12">
           <h1 className="text-3xl font-light text-gray-900 mb-3">Welcome!</h1>
           <div className="w-12 h-0.5 bg-blue-600 mx-auto"></div>
@@ -44,32 +44,33 @@ function LoginPage() {
 
         {/*개발 모드 전용 로그인 폼 */}
         {process.env.NODE_ENV === "development" && (
-          <form className="space-y-6">
-            <div className="space-y-1">
-              <Input
-                type="email"
-                className="w-full px-0 py-3 rounded-none border-0 border-b-2 border-gray-200 bg-transparent  placeholder-gray-400 transition-colors shadow-none"
-                placeholder="Email Address"
-              />
-            </div>
+          // <form className="space-y-6">
+          //   <div className="space-y-1">
+          //     <Input
+          //       type="email"
+          //       className="w-full px-0 py-3 rounded-none border-0 border-b-2 border-gray-200 bg-transparent  placeholder-gray-400 transition-colors shadow-none"
+          //       placeholder="Email Address"
+          //     />
+          //   </div>
 
-            <div className="space-y-1">
-              <Input
-                type="password"
-                className="w-full px-0 py-3 rounded-none border-0 border-b-2 border-gray-200 bg-transparent  placeholder-gray-400 transition-colors shadow-none"
-                placeholder="Password"
-              />
-            </div>
+          //   <div className="space-y-1">
+          //     <Input
+          //       type="password"
+          //       className="w-full px-0 py-3 rounded-none border-0 border-b-2 border-gray-200 bg-transparent  placeholder-gray-400 transition-colors shadow-none"
+          //       placeholder="Password"
+          //     />
+          //   </div>
 
-            <div className="pt-4">
-              <button
-                type="submit"
-                className="w-full bg-black text-white py-3 px-6 rounded-none hover:bg-black/80 transition-colors"
-              >
-                로그인
-              </button>
-            </div>
-          </form>
+          //   <div className="pt-4">
+          //     <button
+          //       type="submit"
+          //       className="w-full bg-black text-white py-3 px-6 rounded-none hover:bg-black/80 transition-colors"
+          //     >
+          //       로그인
+          //     </button>
+          //   </div>
+          // </form>
+          <GoogleSignInButton />
         )}
 
         {/* 임시 계정 발급 및 로그인 */}
--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -4,16 +4,21 @@ import BenchmarkCharts from "./widget/benchmarkCharts";
 import PortfolioList from "./widget/portfolioList";
 import PortfolioPresets from "./widget/portfolioPresets";
 import TrendList from "./widget/trendList";
-import { authClient } from '@/lib/auth-clinet'
+import { authClient } from "@/lib/auth-clinet";
 
 export default function Home() {
-
-  const {data:session} = authClient.useSession()
+  const { data: session } = authClient.useSession();
 
   return (
     <section className="flex flex-col w-full 2xl:w-6/7 gap-10 p-6">
       <div className="flex flex-col">
-        <h1 className="text-3xl font-bold text-gray-900">홈 {session?.user?.name.includes('guest') ? '- guest' : session?.user?.name}</h1>
+        <h1 className="text-3xl font-bold text-gray-900">
+          환영합니다!{" "}
+          {session?.user?.name.includes("guest")
+            ? "- guest"
+            : session?.user?.name}
+          님
+        </h1>
       </div>
 
       <PortfolioList />
